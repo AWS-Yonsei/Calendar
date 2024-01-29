3bd1b02ad6433d3f77156d3979c3a6a913b3b8d2
zephirumwest <111879138+zephirumwest@users.noreply.github.com>
2024-01-30 05:37:05 +0900

    Add files via upload

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,29 +1,27 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-
 import './CalendarApp.css';
 
-const CalendarApp = () => {
+const AppleCalendar = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [memo, setMemo] = useState('');
   const [memos, setMemos] = useState({});
   const [showMemoModal, setShowMemoModal] = useState(false);
+  const [editMode, setEditMode] = useState(false); // 편집 모드 추가
 
   const handleDateClick = (newDate) => {
     setSelectedDate(newDate);
-
-    // 클릭한 날짜에 해당하는 메모를 보여주기
     setShowMemoModal(true);
 
-    // 메모가 있으면 보이게 하고, 없으면 공백의 메모창 보이도록
     if (memos[newDate.toDateString()]) {
       setMemo(memos[newDate.toDateString()]);
     } else {
       setMemo('');
     }
+
+    // 편집 모드를 끝내고 보기 모드로 전환
+    setEditMode(false);
   };
 
   const handleMemoModalClose = () => {
@@ -39,23 +37,30 @@ const CalendarApp = () => {
     setShowMemoModal(false);
   };
 
+  const enterEditMode = () => {
+    setEditMode(true);
+  };
+
   return (
-    <div className="calendar-app">
+    <div className="apple-calendar">
       <div className="modal-container" style={{ display: showMemoModal ? 'flex' : 'none' }}>
         <div className="memo-container">
           <h2>{selectedDate.toDateString()}</h2>
           <div className="memo-editor">
-            <DatePicker selected={selectedDate} onChange={() => {}} />
-            <textarea
-              placeholder="메모를 입력하세요"
-              value={memo}
-              onChange={handleMemoChange}
-            />
-            <button onClick={saveMemo}>메모 저장</button>
-          </div>
-          <div className="memo-display">
-            <h3>{selectedDate.toDateString()} 메모</h3>
-            <p>{memos[selectedDate?.toDateString()]}</p>
+            {editMode ? ( // 편집 모드일 때 텍스트 에디터 보여주기
+              <textarea
+                placeholder="메모를 입력하세요"
+                value={memo}
+                onChange={handleMemoChange}
+              />
+            ) : (
+              <p>{memo}</p> // 보기 모드일 때 텍스트 보여주기
+            )}
+            {editMode ? ( // 편집 모드일 때 "저장" 버튼 보여주기
+              <button onClick={saveMemo}>메모 저장</button>
+            ) : (
+              <button onClick={enterEditMode}>편집</button> // 보기 모드일 때 "편집" 버튼 보여주기
+            )}
           </div>
           <button onClick={handleMemoModalClose}>닫기</button>
         </div>
@@ -77,4 +82,4 @@ const CalendarApp = () => {
   );
 };
 
-export default CalendarApp;
+export default AppleCalendar;
